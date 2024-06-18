6c227e61c1f0a149bb2b6dd5d4418ab1886e0abe
JennieGalmin <Jennie.Galmin@gmail.com>
2024-06-18 14:43:42 +0200

    fetched api from imdb

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Movie {
+  id: string,
+  l: string, 
+  s: string,
+  i: {imageUrl: string}
 }
 
-export default App;
+
+const App = () => {
+
+  const [movies, setMovies] = useState<Movie[]>([])
+
+
+
+  const fetchData = async () => {
+  const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=game';
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': 'e892977ecfmsha4861a2f0dc65ccp10dec6jsn98637e40bbcb',
+      'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+    }
+  };
+  
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    setMovies(result.d)
+  } catch (error) {
+    console.error(error);
+  }
+  }
+
+  useEffect(()=> {
+    fetchData();
+  },[]);
+
+
+  return (
+    <>
+      <div className='header'>
+      <h1>Movie App</h1>
+
+      <input type='text' className='input'></input>
+      <button className='button'>Sök</button>
+      </div>
+      <div className='movieList'>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          {movie.i && <img src={movie.i.imageUrl} alt={movie.l} />}
+          <h2>{movie.l}</h2>
+          <p>{movie.s}</p>
+        </li>
+      ))}
+      </div>
+    </>
+  )
+}
+
+export default App
+
